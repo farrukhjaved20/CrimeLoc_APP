35804f23d9e39a1b5bef5d8f3f39fad45a3c6864
Farrukh Javed <128699725+farrukhjaved20@users.noreply.github.com>
2024-06-23 17:06:11 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/crime_heat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.crime_heat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:crime_heat/constant/constant.dart';
+import 'package:crime_heat/views/authentication/sigin.dart';
+import 'package:crime_heat/views/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CrimeLoc',
+      theme: ThemeData(
+        fontFamily: 'Laila',
+        textSelectionTheme:
+            TextSelectionThemeData(cursorColor: AppColors.bluecolor),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(AppColors.bluecolor),
+                foregroundColor: WidgetStatePropertyAll(AppColors.whitecolor))),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SignInPage(),
+    );
+  }
+}
